078c06514d660b35c8e8c18490ef5bda720f8805
saran87 <saran.kkumar@gmail.com>
2022-04-11 22:28:40 -0400

    Updated changes for git PR approval

--- src/main/java/SeleniumSesions/github_CartPage.java
+++ src/main/java/SeleniumSesions/github_CartPage.java
@@ -5,6 +5,8 @@ public class github_CartPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Github branching logic");
+			
+			System.out.println("Fix issues for PR approval by github reviewer");
 	}
 
 }
